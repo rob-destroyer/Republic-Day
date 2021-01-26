7546a0f6cc91e9165c5a1407d5a1794966d1b9a4
rob-destroyer <62438714+rob-destroyer@users.noreply.github.com>
2021-01-26 09:44:24 +0530

    Update happy.sh

--- happy.sh
+++ happy.sh
@@ -31,33 +31,6 @@ Happy () {
 	echo
 	echo "$( date +"%c" | toilet -f term -F gay -F border )"
 	echo
-	cowsay Happy Republic Day 2020 | lolcat
+	cowsay Happy Republic Day 2021 | lolcat
 	
 printf "\n\n\033[92m	<<<<====>>>> Conguratulation !!! <<<<====>>>> \n\n"
-sleep 0.50
-New
-}
-New () {
-printf "   	<<<===>>> Happy Republic Day 26 january 2020 <<<===>>> \n\n"|lolcat
-printf "\n\n Happy Republic Day 26 january 2020 for all friends from rootedcyber �����\n\n\n"|toilet -f term -F gay
-
-printf "\n\n26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-printf "26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-printf "26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-printf "26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-printf "26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-printf "26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-printf "26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-printf "26 January 2020 || 26 January 2020 || 26 January 2020 || 26 January 2020\n"|lolcat
-printf "Happy Republic Day �����\n\n"|lolcat
-
-Happy
-}
-Happy
